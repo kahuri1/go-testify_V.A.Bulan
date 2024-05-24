e47c7480a7b7af15f4148bf5e963036dc71cdeb3
kahuri1 <vova150820@gmail.com>
2024-05-24 22:35:25 +0300

    2-try

--- precode_test.go
+++ precode_test.go
@@ -3,51 +3,12 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-var cafeList = map[string][]string{
-	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
-}
-
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-	countStr := req.URL.Query().Get("count")
-	if countStr == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("count missing"))
-		return
-	}
-
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong count value"))
-		return
-	}
-
-	city := req.URL.Query().Get("city")
-
-	cafe, ok := cafeList[city]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong city value"))
-		return
-	}
-
-	if count > len(cafe) {
-		count = len(cafe)
-	}
-
-	answer := strings.Join(cafe[:count], ",")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(answer))
-}
 
 // Запрос сформирован корректно, сервис возвращает код ответа 200 и тело ответа не пустое
 func TestMainHandlerWhenOk(t *testing.T) {
@@ -58,19 +19,22 @@ func TestMainHandlerWhenOk(t *testing.T) {
 	handler.ServeHTTP(responseRecorder, req)
 
 	require.Equal(t, responseRecorder.Code, http.StatusOK, "expected status code: %d, got %d", http.StatusOK, responseRecorder.Code)
-	assert.NotEmpty(t, responseRecorder.Body.String())
+	assert.NotEmpty(t, responseRecorder.Body)
 }
 
 // Город, который передаётся в параметре city, не поддерживается. Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа.
 func TestMainHandlerCityIsNotSupported(t *testing.T) {
-	oneCity := "moscow"
-	req := httptest.NewRequest("GET", "/cafe?city=moscow", nil)
+	req := httptest.NewRequest("GET", "/cafe?city=mosscow", nil)
 	city := req.URL.Query().Get("city")
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.Equal(t, oneCity, city, "wrong city value &s", http.StatusBadRequest)
+	_, ok := cafeList[city]
+	if !ok {
+		assert.Equal(t, 400, responseRecorder.Code, "Expected status code 400")
+		assert.Equal(t, "wrong city value", responseRecorder.Body, "wrong city value")
+	}
 
 }
 
